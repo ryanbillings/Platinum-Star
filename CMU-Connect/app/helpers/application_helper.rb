dd25fccf0aca8b5471995f9f4e3424513251577f
Ryan Billings <rbilling@andrew.cmu.edu>
2012-02-25 13:25:58 -0500

    Mac Script

--- CMU-Connect/app/helpers/application_helper.rb
+++ CMU-Connect/app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
     def broadcast(channel, &block)
       message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-<<<<<<< HEAD
-      uri = URI.parse("http://128.237.241.183:9292/faye")
-=======
       uri = URI.parse("http://128.237.237.228:9292/faye")
->>>>>>> added twitter
       Net::HTTP.post_form(uri, :message => message.to_json)
     end
 end
